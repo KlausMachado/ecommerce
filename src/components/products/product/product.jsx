e77b45d955a1f7f01bc133fd9599e498e596b6c0
Carla Machado <carlamrosa438@gmail.com>
2022-08-24 11:54:58 -0300

    creation component product

--- src/components/products/product/product.jsx
+++ src/components/products/product/product.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+} from "@mui/material";
 
-const Product = () => {
-    
-}
+const Product = (product) => {
+    // console.log(product.products.name)
+  return (
+    <Card>
+      <CardMedia title={product.name} image="" component="img" />
+      <CardContent>
+        <div>
+          <Typography variant="h5" gutterBottom>
+            {product.products.name}
+          </Typography>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default Product;
--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -8,12 +8,14 @@ const products = [
     {id: 2, name: 'Macbook', description: 'Apple macbook.'}
 ]
 
-const RenderProducts = () => {
+const RenderProducts = (i) => {
   return (
-    <main>
-      <Grid sx={{ backgroundColor: "blue" }}>
+    <main style={{width: '500px'}}>
+      <Grid key={i} sx={{ backgroundColor: "blue" , width: 500 }}>
+        <h1>Teste</h1>
         {products.map((props) => {
-          return <Grid item key={products.id} xs={12} sm={6} md={4} lg={3}>
+          return <Grid item key={props.id} xs={12} sm={6} md={4} lg={3}>
+            {/* {console.log(props)} */}
             <Product products={props} />
           </Grid>;
         })}
