4cfe0d7a00656224d60d037e161a444fe0d0e56c
Carla Machado <carlamrosa438@gmail.com>
2022-08-25 20:23:38 -0300

    updates product

--- src/components/products/product/product.jsx
+++ src/components/products/product/product.jsx
@@ -7,19 +7,27 @@ import {
   Typography,
   IconButton,
 } from "@mui/material";
+import { AddShoppingCart } from "@mui/icons-material";
 
-const Product = (product) => {
-    // console.log(product.products.name)
+const Product = (products) => {
+  // console.log(product.products.name)
   return (
     <Card>
-      <CardMedia title={product.name} image="" component="img" />
+      <CardMedia title={products.products.name} image="" component="img" />
       <CardContent>
         <div>
           <Typography variant="h5" gutterBottom>
-            {product.products.name}
+            {products.products.name}
           </Typography>
+          <Typography variant="h5">{products.products.price}</Typography>
         </div>
+        <Typography variant="h4">{products.products.description}</Typography>
       </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="Add to cart">
+          <AddShoppingCart />
+        </IconButton>
+      </CardActions>
     </Card>
   );
 };
--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import Grid from "@mui/material/Grid";
 import Product from "./product/product";
 
+//Grid é um componente de colunas
 
 const products = [
-    {id: 1, name: 'Shoes', description: 'Running shoes.'},
-    {id: 2, name: 'Macbook', description: 'Apple macbook.'}
+    {id: 1, name: 'Shoes', description: 'Running shoes.' , price: '100'},
+    {id: 2, name: 'Macbook', description: 'Apple macbook.' , price: '1000'}
 ]
 
 const RenderProducts = (i) => {
   return (
     <main style={{width: '500px'}}>
-      <Grid key={i} sx={{ backgroundColor: "blue" , width: 500 }}>
-        <h1>Teste</h1>
+      <Grid key={i}>
         {products.map((props) => {
           return <Grid item key={props.id} xs={12} sm={6} md={4} lg={3}>
             {/* {console.log(props)} */}
